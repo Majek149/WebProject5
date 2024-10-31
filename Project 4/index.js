ee6a34a7a746a5b6527a0249af9c23d5bd72f1d6
Kehinde M.S <majek149@gmail.com>
2024-10-31 04:34:05 -0700

    Add files via upload

---
+++ Project 4/index.js
@@ -0,0 +1,21 @@
+
+function calculateLoan() {
+    loanAmountValue = document.
+    getElementById("loan-amount").value
+
+    interestRateValue = document.
+    getElementById("interest-rate").value
+
+    MonthsToPayValue = document.
+    getElementById("months-to-pay").value
+
+    interest = (loanAmountValue * 
+        (interestRateValue * 0.01)) / MonthsToPayValue
+
+    MonthlyPayment = (loanAmountValue / 
+       MonthsToPayValue + interest).toFixed(2);
+
+     document.getElementById("payment").innerHTML = 
+     `Monthly Payment: ${MonthlyPayment}`;
+
+}
